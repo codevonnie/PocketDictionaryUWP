1ec9537ca3e6abf2d78cbd9317c6d8e73db1b476
codevonnie <yvonnegrealy@hotmail.com>
2016-12-02 17:47:16 +0000

    Error checking for non-alphabetical characters entered as lookup word

--- PocketDictionary/PocketDictionary/MainPage.xaml.cs
+++ PocketDictionary/PocketDictionary/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using ViewModels;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,16 +32,45 @@ namespace PocketDictionary
         public static String input;
 
         //event listener for when user clicks search button
-        private void searchBtn_Click(object sender, RoutedEventArgs e)
+        private async void searchBtn_Click(object sender, RoutedEventArgs e)
         {
             //convert word to lowercase for api
             input = inputText.Text.ToLower();
-            //create new view model based on user input
-            WordModel = new OtherViewModel(input);
-            //update observable collection
-            Bindings.Update();
-            
+
+            //check input is only alphabetical characters
+            var check = IsAllAlphabetic(input);
+
+            //if check is true, start call to api
+            if (check)
+            {
+                //create new view model based on user input
+                WordModel = new OtherViewModel(input);
+                //update observable collection
+                Bindings.Update();
+            }
+            //if chars entered are not all alphabetical, alert user
+            else
+            {
+                var dialog = new MessageDialog("Please only use alphabetical characters");
+                await dialog.ShowAsync();
+            }
+            check = false;
+
         }
+
+        //check for character types in input value
+        bool IsAllAlphabetic(string value)
+        {
+            foreach (char c in value)
+            {
+                if (!char.IsLetter(c))
+                    return false;
+            }
+
+            return true;
+        }
+
+
     }
 }
 
